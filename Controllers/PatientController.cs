5ba12340265232b56e0443b5eaad3fd678bdf596
nidhi <nidhipradhan54@gmail.com>
2025-10-27 01:55:37 +0530

    first commit

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,34 @@
+﻿using iFlow_crud.repository.Models;
+using iFlow_crud.service.FlowService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//endpoints
+namespace iFlow_crud.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly IFlowService flowService;
+
+        public PatientController(IFlowService _flowService)
+        {
+            flowService = _flowService;
+        }
+        [HttpPost("AddPatient")]
+        public async Task<IActionResult> AddPatient(Patient patient) 
+        {
+            await flowService.AddAsync(patient);
+
+            // Success response
+            return Ok(new { success = true, message = "Patient added successfully.", data = patient });
+        }
+        //public async Task<IActionResult> Add(Patient patient)
+        //{
+        //    FirstName = patient.FirstName;
+        //    return patient;
+        //}
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,47 @@
+﻿using iFlow_crud.repository.FlowRepo;
+using iFlow_crud.repository.Models;
+using iFlow_crud.service.FlowService;
+using Microsoft.EntityFrameworkCore;
+
+namespace iFlow_crud.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration) => _configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IFlowService, FlowService>();
+            services.AddScoped<IFlowRepository, FlowRepository>();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            // enable auth middleware
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
